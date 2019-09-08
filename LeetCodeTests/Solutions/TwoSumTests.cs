596ab58d8e5d3e8983a5b84bc702f5d0c501a449
Jessica <jkhaynes2390@gmail.com>
2019-09-08 17:21:27 -0500

    add tests

---
+++ LeetCodeTests/Solutions/TwoSumTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class TwoSumTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+
+    public class TwoSumInput {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData().Returns()
+            }
+        }
+    }
+}
