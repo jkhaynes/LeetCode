df585420d682f6c1447ce7890725ad5e1e561e5c
Jessica <jkhaynes2390@gmail.com>
2019-09-08 17:22:00 -0500

    add twosum

---
+++ LeetCode/Solutions/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Solutions
+{
+    public static class TwoSum
+    {
+        public static int[] GetTwoNumbersThatAddToTarget(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+                if (dict.ContainsKey(complement))
+                {
+                    return new int[] { dict[complement], i };
+                }
+                else if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
