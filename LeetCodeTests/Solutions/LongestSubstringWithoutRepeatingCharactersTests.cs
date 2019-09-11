6b8a6560ffb59e15fb35e89224081175a041c7d7
Jessica Haynes <Jessica.Haynes@growersedge.com>
2019-09-10 21:02:27 -0500

    working solution to longestsubstringwithoutrepeatingcharacters

---
+++ LeetCodeTests/Solutions/LongestSubstringWithoutRepeatingCharactersTests.cs
@@ -0,0 +1,36 @@
+﻿using LeetCode.Solutions;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    public class LongestSubsringWithoutRepeatingCharactersTests
+    {
+        [Test, TestCaseSource(typeof(LongestStringWithoutRepeatingCharactersInput), "TestCases")]
+        public int GetTwoNumbersThatAddToTargetTest(string s)
+        {
+            return LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(s);
+        }
+
+        [Test]
+        public void GetTwoNumbersThatAddToTargetNullExceptionTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => LongestSubstringWithoutRepeatingCharacters.LengthOfLongestSubstring(null));
+        }
+    }
+
+    public class LongestStringWithoutRepeatingCharactersInput
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("abcabcbb").Returns(3);
+                yield return new TestCaseData("aabaab!bb").Returns(3);
+            }
+        }
+    }
+}
